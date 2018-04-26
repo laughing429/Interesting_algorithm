ff1ad8c2e97a9a813ee50bdd89b733175fca060b
laughing429 <327983097@qq.com>
2018-04-26 09:31:54 +0800

    add redbag

---
+++ 抢红包算法/redbag.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# Create on: 2018/4/25 0025 14:18 
+# Author: Lyu 
+# Annotation:抢红包算法实现
+
+# 如果说每个人抢红包的随机数是剩下金额的随机数，那么每个人抢到红包的均值就不一样。所以下面有两种算法
+
+import random
+import numpy as np
+from collections import Counter
+
+def doubleMean(totalAmount, peopleNum):
+    """
+    二倍均值法
+    totalAmount: 总的金额
+    peopleNum: 抢红包总人数
+    """
+    useAmount = 0
+    testAll = []
+    for p in range(peopleNum-1):
+        restAmount = totalAmount - useAmount
+        restPeople = peopleNum - p
+        amount = random.uniform(0.01, restAmount/restPeople*2)
+        amount = round(amount, 2)
+        print "抢到的金额：", amount
+        useAmount += amount
+        testAll.append(amount)
+    # 最后一个人的红包
+    amount = totalAmount - useAmount
+    print "抢到的金额：", amount
+    testAll.append(amount)
+    print "总共抢到的金额数：", sum(testAll)
+
+class cutLine(object):
+    """
+    线段切割法
+    """
+    def __init__(self, totalAmount, peopleNum):
+        self.totalAmount = totalAmount
+        self.peopleNum = peopleNum
+        self.cut()
+
+    def repeatHandle(self, cutPoint):
+
+        pass
+
+    def cut(self):
+        testAll = []
+        cutPoint = np.random.uniform(0.01, self.totalAmount, self.peopleNum-1)
+        cutPoint = np.round(cutPoint, decimals=2)
+        cutPoint.sort()
+        cutPoint = cutPoint.tolist()
+
+        # 检查是否有重复的情况
+
+
+        cutPoint.insert(0, 0)
+        cutPoint.insert(self.peopleNum, self.totalAmount)
+        for point in np.diff(cutPoint):
+            print "抢到的金额：", point
+            testAll.append(point)
+        print "总共抢到的金额数：", sum(testAll)
+
+
+if __name__ =='__main__':
+    # doubleMean(1000, 10)
+    cutLine(1000, 10)
+
